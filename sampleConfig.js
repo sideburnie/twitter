d1ee1f7625965e629eb3dd2b7da9cd60893e5311
sideburnie <bernie.rihn@gmail.com>
2016-02-05 20:31:26 -0800

    add sample config file

---
+++ sampleConfig.js
@@ -0,0 +1,24 @@
+"use strict"
+
+var config = {
+  emailOptions: {
+    user: 'robot',
+    pass: '',
+    emailDomain: 'hashplex.com',
+    mailServerFqdn: 'smtp.gmail.com',
+    mailServerPort: 465,
+    fromEmail: 'robot@hashplex.com',
+    toEmail: '',
+    subject: 'UNFOLLOWED AGAIN',
+    plaintextMessage: '',
+    htmlMessage: ''
+  }, 
+  twitOptions: {
+      consumer_key:         ''
+    , consumer_secret:      ''
+    , access_token:         ''
+    , access_token_secret:  ''
+  },
+  checkIntervalSeconds: 180 
+}
+module.exports = config
